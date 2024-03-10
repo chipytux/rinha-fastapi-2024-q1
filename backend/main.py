c1ba00205676947ab5a5d8735b5544fa019d8712
Dino Aguilar <chipytux@users.noreply.github.com>
2024-03-10 17:50:59 -0300

    remove warmup

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,6 @@
-import logging
 import os
 from datetime import datetime
 from enum import Enum
-from time import sleep
 from typing import Annotated
 from typing import AsyncGenerator
 
@@ -27,7 +25,7 @@ from sqlalchemy import (
     DateTime,
     AsyncAdaptedQueuePool,
 )
-from sqlalchemy import text, insert, select
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
     async_sessionmaker,
@@ -53,20 +51,6 @@ engine: AsyncEngine = create_async_engine(
 )
 
 SESSION_MAKER = async_sessionmaker(engine, expire_on_commit=False)
-
-
-@app.on_event("startup")
-async def startup_event():
-    async with SESSION_MAKER() as session:
-        for i in range(10):
-            try:
-                await session.execute(text("SELECT 1"))
-            except ConnectionRefusedError as cer:
-                logging.error(cer)
-                sleep(1)
-                continue
-            else:
-                break
 
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
